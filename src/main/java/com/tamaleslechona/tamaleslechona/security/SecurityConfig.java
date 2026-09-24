b07fcc9d316f4570dee44a1c458d3b5cca566676
Azardep00 <sebastianpedrazajft@gmail.com>
2026-09-23 19:07:15 -0500

    Agrega dominio de admin-tamales a CORS

--- src/main/java/com/tamaleslechona/tamaleslechona/security/SecurityConfig.java
+++ src/main/java/com/tamaleslechona/tamaleslechona/security/SecurityConfig.java
@@ -74,7 +74,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200", "http://localhost:5173", "https://cliente-tamales.vercel.app"));
+        config.setAllowedOrigins(List.of("http://localhost:4200", "http://localhost:5173", 
+                                "https://cliente-tamales.vercel.app", "https://admin-tamales.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
 
